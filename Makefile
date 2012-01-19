cd0102ce0180014989dc0486738f59d195395afe
Jingwei <liujingwei@gmail.com>
2012-01-19 21:09:57 +0800

    * wrote an improved version of brutal force string searching * wrote a faster int-to-string conversion function * removed stringencoder library dependency * updated comments

--- Makefile
+++ Makefile
@@ -1,7 +1,20 @@
 all: slopi 
 
+debug: slopi.c
+	clear
+	gcc -pg -Wall -o slopi slopi.c -lm -pthread
+
 clean: slopi 
 	rm slopi
 
-slopi: slopi.c Makefile
-	gcc -O3 -o slopi slopi.c modp_numtoa.c -lm -pthread
+slopi: slopi.c
+	clear
+	gcc -O3 -o slopi slopi.c -lm -pthread
+
+sold: slop-cachesize=10i.c
+	gcc -O3 -o sold "slopi-cachesize=10.c" modp_numtoa.c -lm -pthread
+
+run: slopi.c Makefile
+	gcc -O3 -o slopi slopi.c -lm -pthread
+	clear
+	./slopi
--- slopi.c
+++ slopi.c
@@ -1,8 +1,7 @@
 /* Find self-locating numbers in numbers, esp. in Pi.
- * modp_numtoa is from stringencoders at 
- *     https://code.google.com/p/stringencoders/wiki/NumToA
  *
- *  Last Modified: Fri 06 Jan 2012 11:35:35 PM (tweaked/forked by @th507)
+ *  Last Modified: Thu 19 Jan 2012 08:55:04 PM
+ *  Tweaked/forked by @th507
  *
  * Copyright (c) Yongzhi Pan, Since Dec 7 2011
  *
@@ -10,7 +9,6 @@
  */
 
 #define _GNU_SOURCE
-#include "modp_numtoa.h"
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
@@ -19,211 +17,84 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-// seems that this file is not needed
-//#include <sys/types.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+// for uint64_t
+#include <inttypes.h>
 
-// COMMENTS
-/*
+/* my notebook is not running 64bit OS :(
 #if LONG_MAX <= 10000000
 #error Your compiler`s long int type is too small!
 #endif
 */
 
-// redundant, but what the heck...
-#define SEEK_SET 0
+// have to be larger than LOOP
+#define CACHESIZE 10000
 
 // Number of bytes to bypass. 
 // in order to make 1 the 1st digit of pi (3.1415...), we set OFFSET to 1.
 // This is DIFFERENT from Peter's definition!
 #define OFFSET 1
-#define CACHESIZE 10
 #define err_exit_en(en, msg) \
   do { errno=en; perror(msg); exit(EXIT_FAILURE); } while (0)
 #define err_exit(msg) \
   do { perror(msg); exit(EXIT_FAILURE); } while(0)
 
-int fast_log10_ceil (long v) {
+int fast_log10_minus1 (long v) {
   // needs to expand for larger cases
-  return (v >= 1000000000) ? 10 : (v >= 100000000) ? 9 : (v >= 10000000) ? 8 : 
-    (v >= 1000000) ? 7 : (v >= 100000) ? 6 : (v >= 10000) ? 5 : 
-    (v >= 1000) ? 4 : (v >= 100) ? 3 : (v >= 10) ? 2 : 1;
+  return (v >= 10000000000) ? 10 : (v >= 1000000000) ? 9 : 
+    (v >= 100000000) ? 8 : (v >= 10000000) ? 7 : (v >= 1000000) ? 6 : 
+    (v >= 100000) ? 5 : (v >= 10000) ? 4 : (v >= 1000) ? 3 : 
+    (v >= 100) ? 2 : (v >= 10) ? 1 : 0;
 }
 
-
-// Knuth-Morris-Pratt
-void prefixKMP (char *pattern, int patternLength, int * kmpPi) {
-  int i, j;
-
-  i = 0;
-  j = kmpPi[0] = -1;
-
-  // in the first run, when the second while is finished
-  // the j = 0, which agrees with KMP algorithm
-
-  while (i < patternLength) {
-    while (j > -1 && pattern[i] != pattern[j])
-      j = kmpPi[j];
-
-    i++;
-    j++;
-
-    if(pattern[i] == pattern[j])
-      kmpPi[i] = kmpPi[j];
-    else
-      kmpPi[i] = j;
+// replacing mod_litoa
+void ascendingIntToString(char * t, int i, int len) {
+  if (i > len + 1 || t[i] < 48) {
+    t[i] = 49;
+    return;    
   }
-}
-
-void searchInKMP(char * pattern, int patternLength, 
-    char * text, int textLength, int * addr) {
-
-  int i, j;
-  //int kmpPi[patternLength];
-  // this is somewhat unreliable because we assumed sizeof(int) = 4
-  int * kmpPi = (int *)malloc((patternLength << 2));
-
-  // preprocessing
-  i = 0;
-  j = kmpPi[0] = -1;
-
-  // in the first run, when the second while is finished
-  // the j = 0, which agrees with KMP algorithm
-  prefixKMP(pattern, patternLength, kmpPi);
-
-  // searching
-  i = j = 0;
-  while (j < textLength) {
-    while (i > -1 && pattern[i] != text[j])
-      i = kmpPi[i];
-    i++;
-    j++;
-    if (i >= patternLength ) {
-      // binary representation of matches
-      *addr += (1 << (j - i));
-      i = kmpPi[i];
-    }
+  
+  if (t[i] == 57) {
+    t[i] = 48;
+    ascendingIntToString(t, i + 1, len);
   }
-
-  free(kmpPi);
+  else
+    t[i] += 1;
 }
 
-
-// Boyer-Moore
-#define max(a, b) ((a < b) ? b : a)
-void preBmBc(char *x, int m, int bmBc[]) {
-  int i;
-
-  for (i = 0; i < CACHESIZE; ++i)
-    bmBc[i] = m;
-  for (i = 0; i < m - 1; ++i)
-    bmBc[x[i]] = m - i - 1;
-}
-
-void suffixes(char *x, int m, int * suff) {
-  int f, g, i;
-
-  suff[m - 1] = m;
-  g = m - 1;
-  for (i = m - 2; i >= 0; --i) {
-    if (i > g && suff[i + m - 1 - f] < i - g)
-      suff[i] = suff[i + m - 1 - f];
-    else {
-      if (i < g)
-        g = i;
-      f = i;
-      while (g >= 0 && x[g] == x[g + m - 1 - f])
-        --g;
-      suff[i] = f - g;
+// improved Brutal Force search
+// skip string search if index digit mismatches.
+// pattern is a reversed string
+void searchByIndex(char * p, int m, char * t) {
+  int i, j = 10, k = m - 1, l;
+  while (j) {
+    j--;
+    if (t[m + j] - 48 != j) continue;
+    for (i = 0; i <= m && p[k - i] == t[i + j]; ++i);
+    if (i >= m) {
+      for (l = k; l >= 0; l--)
+        printf("%c", p[l]);
+      printf("%d\n", j);      
     }
   }
 }
 
-void preBmGs(char *x, int m, int bmGs[]) {
-  int i, j, suff[m];
-
-  suffixes(x, m, suff);
-
-  for (i = 0; i < m; ++i)
-    bmGs[i] = m;
-  j = 0;
-  for (i = m - 1; i >= 0; --i)
-    if (suff[i] == i + 1)
-      for (; j < m - 1 - i; ++j)
-        if (bmGs[j] == m)
-          bmGs[j] = m - 1 - i;
-  for (i = 0; i <= m - 2; ++i)
-    bmGs[m - 1 - suff[i]] = m - 1 - i;
-
-  free(suff);
-}
-
-void searchInBM(char *x, int m, char *y, int n, int * addr) {
-  int i, j, bmGs[m], bmBc[CACHESIZE];
-
-  /* Preprocessing */
-  preBmGs(x, m, bmGs);
-  preBmBc(x, m, bmBc);
-
-  /* Searching */
-  j = 0;
-  while (j <= n - m) {
-    for (i = m - 1; i >= 0 && x[i] == y[i + j]; --i);
-    if (i < 0) {
-      // binary representation of matches      
-      *addr += (1 << j);
-      j += bmGs[0];
-    }
-    else
-      j += max(bmGs[i], bmBc[y[i + j]] - m + 1 + i);
-  }
-}
-
-
-// Brutal Force
-void searchInBF(char * pattern, int patternLength, 
-    char * text, int textLength, int *addr) {
-  int i, j;
-
-  /* Searching */
-  for (j = 0; j <= textLength - patternLength; ++j) {
-    for (i = 0; i < patternLength && pattern[i] == text[i + j]; ++i);
-    if (i >= patternLength)
-      // binary representation of matches
-      *addr += (1 << j);
-  }
-}
 
 static void * find(void * file) {
-  int s;
-  // set CPU affinity
-  // FIXME fixed two threads onto the same core, slows down.
-  /*
-     cpu_set_t set;
-     CPU_ZERO(&set);
-     CPU_SET(1, &set);
-     s = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &set);
-     if(s != 0)
-     err_exit_en(s, "pthread_setaffinity_np");
-     */
-
   char * infile = (char *)file;
   struct stat stat_buf;
   int fd;
   off_t N;
   //register off_t n = 1;
   int maxlen;
-  
-  // maybe it's not needed?
-  // num_read = read(...
-  //int num_read;
 
   // get file size
   stat(infile, &stat_buf);
   N = stat_buf.st_size;
 
-  maxlen = fast_log10_ceil(N);
+  maxlen = fast_log10_minus1(N) + 1;
 
   // open pi data file
   fd = open(infile, O_RDONLY);
@@ -233,109 +104,92 @@ static void * find(void * file) {
   // according to 
   // echo "" | gcc -E -dM -c - | grep linux
   // we should check for __linux, __linux__, __gnu_linux__, linux
-  // what the heck...
+  // we only cover the common case
   #ifdef __linux__
-    s = posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL);
-    if(s != 0)
-      err_exit_en(s, "posix_fadvise");
+  int s;  
+  s = posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL);
+  if(s != 0)
+    err_exit_en(s, "posix_fadvise");
   #endif 
 
   // failed to perform 'lseek'
   if (lseek(fd, OFFSET, SEEK_SET) == (off_t)-1)
     err_exit("lseek");
 
-  // mayber it's not needed?
-  // k = lseek(...
-  //long k;
-
-  // create a buffer from string comparison
-  // this method sometimes results in error 
-  // like 'num_read is 1415926535?' on Mac
-  //char buf[CACHESIZE + maxlen];
-  //char numberString[maxlen];
-
   // this works fine on Mac but crash on Linux
-  char * buf = (char *)malloc(CACHESIZE + maxlen);
-  char * numberString = (char *)malloc(maxlen);
+  char * cache = (char *)malloc(CACHESIZE + maxlen + 1);
+  char * numberString = (char *)malloc(maxlen + 1);
 
-  int matches = 0;
-  // make it a 'global variable'
-  int * addressOfMatches = &matches;
+  int j, len = 0;
 
-  int i, len = 1;
-  
-  // this method cannot be used for j < CACHESIZE
-  long j = CACHESIZE;
-  while (j < N) { // buf never gets reset, maybe it OUGHT TO
-    lseek(fd, (j+OFFSET), SEEK_SET);
+  *numberString = 48; // '0'
 
-    // leaving 1 bit for \0
-    read(fd, buf, len+CACHESIZE);
+  uint64_t n = 0;
 
-    // reduce the number of calculation
-    if (buf[0] == '1') len = fast_log10_ceil(j) - 1;
+  while (n < N) { // buf never gets reset, maybe it OUGHT TO
+    lseek(fd, (n + OFFSET), SEEK_SET);
+    read(fd, cache, len + CACHESIZE);// read to a larger cache
 
-    // search for the number except the last digit
-    modp_litoa10((j/CACHESIZE), numberString);
+    len = fast_log10_minus1(n);
 
-    // to be safe
-    * addressOfMatches = 0;
+    // find the the string except the last digit
+    j = 0;
+    while (j < CACHESIZE) {
+      // for the first CACHESIZE loop
+      if (!n)
+        len = fast_log10_minus1(j);
 
-    searchInKMP(numberString, len, buf, len+CACHESIZE, addressOfMatches);
+      // search through cache[j] to cache[j+len+10]
+      searchByIndex(numberString, len, cache + j);
 
-    i = CACHESIZE;	
-    while (i) {
-      i--;      
-      // if match found
-      if (matches >> i & 1 && buf[len+i] - '0' == i)
-        printf("%s%d\n", numberString, i);
+      j += 10;
+
+      // update the numberString (index/offset except the last digit)
+      // we do NOT reverse the string, nor do we append '\0'
+      ascendingIntToString(numberString, 0, len);
     }
-    j += CACHESIZE;
+    n += CACHESIZE;
   }
 
-  free(buf);
+  free(cache);
   free(numberString);
+
+  return NULL;
 }
 
 int main(int argc, char *argv[]) {
-  int num_cpu = sysconf(_SC_NPROCESSORS_ONLN);
+  int num_cpu = 1;//sysconf(_SC_NPROCESSORS_ONLN);
   if (num_cpu == -1)
     err_exit("sysconf"); 
   printf("CPU number: %d\n", num_cpu);
   //char *files[num_cpu];
-  const char * const files[2] = {"xac", "xad"};
+  const char * const files[2] = {"../../../pi-billion.txt", ""};
   pthread_t tid[num_cpu];
   int i;
   //int j;
   int s;
 
-    if(num_cpu == 2) {
-        // FIXME initialize once
-        // FIXME get file names from argv
-        //files[0] = "xaa";
-        //files[1] = "xab";
-        ;
-    }
+  if(num_cpu == 2) {
+    // FIXME initialize once
+    // FIXME get file names from argv
+    //files[0] = "xaa";
+    //files[1] = "xab";
+    ;
+  }
 
-    /*
-    for(j=0; j<argc-1; j++) {
-        files[j][0] = argv[2*j+1];
-        files[j][1] = argv[2*j+2];
-    }
-    */
-
-  for(i=0; i<num_cpu; i++) {
+  for(i = 0; i < num_cpu; i++) {
     s = pthread_create(&tid[i], NULL, find, (void *)files[i]);
     if(s != 0)
       err_exit_en(s, "pthread_create");
   }
 
   /* now wait for all threads to terminate */
-  for(i=0; i<num_cpu; i++) {
+  for(i = 0; i < num_cpu; i++) {
     s = pthread_join(tid[i], NULL);
     if(s != 0)
       err_exit_en(s, "pthread_join");
   }
 
+  printf("\n");
   return 0;
 }
